8f5e7fcc20ecbe5f69507087986dad1477eb719f
mbalattpsc <72672258+Bauaser-kun@users.noreply.github.com>
2026-01-02 15:28:58 +0100

    initial for showing lists

--- app/src/main/java/com/PortalGames/PortalDEX/PDFManager.java
+++ app/src/main/java/com/PortalGames/PortalDEX/PDFManager.java
@@ -1,4 +1,31 @@
 package com.PortalGames.PortalDEX;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PDFManager {
+    public List<String> getPDFListFromAssets(Context context) {
+        List<String> pdfs = new ArrayList<>();
+        AssetManager manager = context.getAssets();
+
+        try {
+            String[] fileNames = manager.list("");
+            if (fileNames != null) {
+                for (String file: fileNames) {
+                    if (file.toLowerCase().endsWith(".pdf")) {
+                        pdfs.add(file);
+                    }
+                }
+            }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return pdfs;
+    }
 }
